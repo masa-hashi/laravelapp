6e67fa05de5f8bacf8444de6e9095ca5dad6efb2
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-05 14:23:50 +0900

    6-27

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,6 @@ Route::post('person/add', 'PersonController@create');
 
 Route::get('person/edit', 'PersonController@edit');
 Route::post('person/edit', 'PersonController@update');
+
+Route::get('person/del', 'PersonController@delete');
+Route::post('person/del', 'PersonController@remove');

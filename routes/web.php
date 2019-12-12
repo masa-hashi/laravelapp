47e01c567201af303bf494deeba450bcdcb27b2e
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-12 17:20:53 +0900

    7-30

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('top', function () {
     return view('top');
 });
 
-Route::get('hello', 'HelloController@index')->middleware('helo');
+Route::get('hello', 'HelloController@index')->middleware('auth');
 
 Route::post('hello', 'HelloController@post');
 /*

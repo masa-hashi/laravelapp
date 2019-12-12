b2feabe3e68002baf5e5671f0ab0875f5275f9e4
Masashi Hashimoto <mhashimo200@gmail.com>
2019-12-12 16:39:24 +0900

    7-23

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,8 +13,12 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-      $items = DB::table('people')->simplePaginate(5);
-      return view('hello.index', ['items'=> $items]);
+      $sort = $request->sort;
+      // $items = DB::table('people')->simplePaginate(5);
+      // return view('hello.index', ['items'=> $items]);
+        $items = Person::orderby($sort, 'asc')->simplePaginate(5);
+        $param = ['items' => $items, 'sort' => $sort];
+        return view('hello.index', $param);
     }
 
     public function post(Request $request)
